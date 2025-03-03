307dde56c04526928a2b1890c8878f5d08612b24
Enrique Ajin <enriquesaon@gmail.com>
2025-03-03 14:23:27 +0100

    Update sql script replacing TEXT for VARCHAR, adding named primary & foreign key constraints.

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -17,15 +17,15 @@ DROP TABLE IF EXISTS "users";
 CREATE TABLE "users" (
     "user_id" bigint NOT NULL,
     "display_name" VARCHAR(50),
-    "email" TEXT NOT NULL UNIQUE,
-    "password_hash" TEXT NOT NULL,
+    "email" VARCHAR(150) NOT NULL UNIQUE,
+    "password_hash" VARCHAR(100) NOT NULL,
     "streak_count" INTEGER NOT NULL,
     "last_pomodoro_date" TIMESTAMP,
-    "avatar_url" TEXT,
+    "avatar_url" VARCHAR(255),
     "is_active" BOOLEAN NOT NULL,
     "created_at" TIMESTAMP NOT NULL,
     "updated_at" TIMESTAMP NOT NULL,
-    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+    CONSTRAINT "pk_users" PRIMARY KEY ("user_id")
 );
 
 DROP TABLE IF EXISTS "categories";
@@ -34,22 +34,22 @@ CREATE TABLE "categories" (
     "category_name" VARCHAR(20) NOT NULL,
     "created_at" TIMESTAMP NOT NULL,
     "updated_at" TIMESTAMP NOT NULL,
-    CONSTRAINT "categories_pkey" PRIMARY KEY ("user_id", "category_name")
+    CONSTRAINT "pk_categories" PRIMARY KEY ("user_id", "category_name"),
+    CONSTRAINT "fk_categories_user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("user_id")
 );
 
 DROP TABLE IF EXISTS "status";
 CREATE TABLE "status" (
     "status_id" smallint NOT NULL,
     "name" VARCHAR(15) NOT NULL UNIQUE,
-    CONSTRAINT "status_pkey" PRIMARY KEY ("status_id")
+    CONSTRAINT "pk_status" PRIMARY KEY ("status_id")
 );
 
 DROP TABLE IF EXISTS "priorities";
 CREATE TABLE "priorities" (
     "priority_id" smallint NOT NULL,
     "name" VARCHAR(15) NOT NULL UNIQUE,
-    CONSTRAINT "priorities_pkey" PRIMARY KEY ("priority_id")
-
+    CONSTRAINT "pk_priorities" PRIMARY KEY ("priority_id")
 );
 
 DROP TABLE IF EXISTS "tasks";
@@ -58,15 +58,18 @@ CREATE TABLE "tasks" (
     "user_id" bigint NOT NULL,
     "category_name" VARCHAR(20) NOT NULL,
     "title" VARCHAR(100) NOT NULL,
-    "description" TEXT,
+    "description" VARCHAR(255),
     "due_date" TIMESTAMP NOT NULL,
     "status_id" smallint NOT NULL,
     "target_pomodoros" smallint NOT NULL,
     "priority_id" smallint NOT NULL,
     "created_at" TIMESTAMP NOT NULL,
     "updated_at" TIMESTAMP NOT NULL,
-    CONSTRAINT "tasks_pkey" PRIMARY KEY ("task_id")
-
+    CONSTRAINT "pk_tasks" PRIMARY KEY ("task_id"),
+    CONSTRAINT "fk_tasks_user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("user_id"),
+    CONSTRAINT "fk_tasks_status_id" FOREIGN KEY ("status_id") REFERENCES "status" ("status_id"),
+    CONSTRAINT "fk_tasks_priorities_id" FOREIGN KEY ("priority_id") REFERENCES "priorities" ("priority_id"),
+    CONSTRAINT "fk_tasks_user_id_category_name" FOREIGN KEY ("user_id", "category_name") REFERENCES "categories" ("user_id", "category_name")
 );
 
 DROP TABLE IF EXISTS "pomodoros";
@@ -76,5 +79,7 @@ CREATE TABLE "pomodoros" (
     "task_id" bigint NOT NULL,
     "ended_at" TIMESTAMP NOT NULL,
     "started_at" TIMESTAMP NOT NULL,
-    CONSTRAINT "pomodoros_pkey" PRIMARY KEY ("pomodoro_id")
+    CONSTRAINT "pk_pomodoros" PRIMARY KEY ("pomodoro_id"),
+    CONSTRAINT "fk_pomodoros_user_id" FOREIGN KEY ("user_id") REFERENCES "users" ("user_id"),
+    CONSTRAINT "fk_pomodoros_task_id" FOREIGN KEY ("task_id") REFERENCES "tasks" ("task_id")
 );
