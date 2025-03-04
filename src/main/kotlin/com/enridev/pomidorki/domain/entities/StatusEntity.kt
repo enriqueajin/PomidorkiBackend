cc3b5fb84705a46a45c916d71c74963411938dd6
Enrique Ajin <enriquesaon@gmail.com>
2025-03-04 11:51:19 +0100

    Create StatusEntity within domain > entities.

---
+++ src/main/kotlin/com/enridev/pomidorki/domain/entities/StatusEntity.kt
@@ -0,0 +1,16 @@
+package com.enridev.pomidorki.domain.entities
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "status")
+data class StatusEntity(
+    @Id
+    @Column(name = "status_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "status_id_seq")
+    @SequenceGenerator(name = "status_id_seq", allocationSize = 1)
+    val id: Int,
+
+    @Column(nullable = false, unique = true)
+    val name: String
+)
