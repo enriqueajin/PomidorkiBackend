c83e3af11d16d5842d66b7a5ec8f2761d619d4db
Enrique Ajin <enriquesaon@gmail.com>
2025-03-08 08:56:31 +0100

    test(service-create-status): Create StatusServiceImpl test cases for the 'create' method.
    Test 1: test that create status with ID throws IllegalArgumentException.
    Test 2: test that create status persists the status in the database.

--- src/test/kotlin/com/enridev/pomidorki/services/impl/StatusServiceImplTest.kt
+++ src/test/kotlin/com/enridev/pomidorki/services/impl/StatusServiceImplTest.kt
@@ -5,8 +5,10 @@ import com.enridev.pomidorki.testStatusEntityA
 import jakarta.transaction.Transactional
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 
 @SpringBootTest
 @Transactional
@@ -28,6 +30,27 @@ class StatusServiceImplTest @Autowired constructor(
         val statusList = underTest.list()
         assertThat(statusList).isNotEmpty()
         assertThat(statusList).isEqualTo(expected)
+    }
+
+    @Test
+    fun `test that create status with ID throws IllegalArgumentException`() {
+        assertThrows<IllegalArgumentException> {
+            val existingStatus = testStatusEntityA(333)
+            underTest.create(existingStatus)
+        }
+    }
+
+    @Test
+    fun `test that create status persists the status in the database`() {
+        val savedStatus = statusRepository.save(testStatusEntityA())
+        assertThat(savedStatus.id).isNotNull()
+
+        val retrievedStatus = statusRepository.findByIdOrNull(savedStatus.id!!)
+        assertThat(retrievedStatus).isNotNull()
+
+        assertThat(retrievedStatus!!).isEqualTo(
+            testStatusEntityA(savedStatus.id)
+        )
     }
 
 }
