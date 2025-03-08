b60731d52776ef1c5806f64c269214ac577cf41c
Enrique Ajin <enriquesaon@gmail.com>
2025-03-08 08:46:28 +0100

    test(service-list-status): Create StatusServiceImpl test cases for the 'list' method.
    Test 1: test that list status return an empty list when no status in the database.
    Test 2: test that list status return a list of status when status present in the database.

--- src/test/kotlin/com/enridev/pomidorki/services/impl/StatusServiceImplTest.kt
+++ src/test/kotlin/com/enridev/pomidorki/services/impl/StatusServiceImplTest.kt
@@ -1,7 +1,10 @@
 package com.enridev.pomidorki.services.impl
 
 import com.enridev.pomidorki.repositories.StatusRepository
+import com.enridev.pomidorki.testStatusEntityA
 import jakarta.transaction.Transactional
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -11,5 +14,20 @@ class StatusServiceImplTest @Autowired constructor(
     private val underTest: StatusServiceImpl,
     private val statusRepository: StatusRepository
 ) {
+
+    @Test
+    fun `test that list status return an empty list when no status in the database`() {
+        val statusList = underTest.list()
+        assertThat(statusList).isEmpty()
+    }
+
+    @Test
+    fun `test that list status return a list of status when status present in the database`() {
+        val savedStatus = statusRepository.save(testStatusEntityA())
+        val expected = listOf(savedStatus)
+        val statusList = underTest.list()
+        assertThat(statusList).isNotEmpty()
+        assertThat(statusList).isEqualTo(expected)
+    }
 
 }
