52fcb48ebd5c1d14fffb790a81930e7ab86a0d89
Enrique Ajin <enriquesaon@gmail.com>
2025-03-09 13:58:56 +0100

    fix(user-dto): Make 'lastPomodoroDate' data type OffsetDateTime.

--- src/main/kotlin/com/enridev/pomidorki/domain/dto/UserDto.kt
+++ src/main/kotlin/com/enridev/pomidorki/domain/dto/UserDto.kt
@@ -1,12 +1,14 @@
 package com.enridev.pomidorki.domain.dto
 
+import java.time.OffsetDateTime
+
 data class UserDto(
     val id: Long?,
     val name: String,
     val email: String,
     val password: String,
     val streakCount: Int,
-    val lastPomodoroDate: String,
+    val lastPomodoroDate: OffsetDateTime,
     val avatarUrl: String,
     val firebaseUid: String,
     val isActive: Boolean
